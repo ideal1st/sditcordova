120f753bced94f5acc5e2e776ad1742480b85691
ideal1st <ideal1st.here@googlemail.com>
2025-10-21 07:31:41 +0700

    Added jQuery and Slick Slider.

---
+++ assets/js/slider.js
@@ -0,0 +1,13 @@
+// Initialize slick on the .slick-container class
+$('.slider').slick({
+    // Your slick-slider settings go here
+    dots: true,
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    adaptiveHeight: true,
+    autoplay: false,
+    autoplaySpeed: 0,
+    cssEase: 'linear'
+});
